60a38bfea7a32383ee538d15b1743ecd395f0313
Pietrodon <donvitopietro00@gmail.com>
2022-02-19 17:49:49 +0100

    creazione if per stabilere  il livello di difficolta scelto dall'utente

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-let button = document.getElementById('start-game')
+const button = document.getElementById('start-game')
+const sceltaLivello = document.getElementById('selec-level')
+
+
 button.addEventListener('click', function(){
-    
+    // console.log(sceltaLivello.value)
+
+    if(sceltaLivello.value === 'easy'){
+        console.log('caso1')
+    } else if(sceltaLivello.value === 'normal'){
+        console.log('caso2')
+    } else{
+        console.log('caso3')
+    }
 })
