9c8bbac6931abb6c007cb8a07983d7dfbe2d316c
Pietrodon <donvitopietro00@gmail.com>
2022-02-19 18:02:53 +0100

    ciclo for per creare i numeri

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+//richiamo elementi html
 const button = document.getElementById('start-game')
 const sceltaLivello = document.getElementById('selec-level')
-
-
+const griglia = document.getElementsByClassName('.grid')
+// ascolto click sul bottone play
 button.addEventListener('click', function(){
     // console.log(sceltaLivello.value)
 
+//verifica del livello scelto 
     if(sceltaLivello.value === 'easy'){
-        console.log('caso1')
+        for (let i = 1; i <101; i++) {
+            console.log([i]);
+            
+        }
     } else if(sceltaLivello.value === 'normal'){
-        console.log('caso2')
+        for (let i = 1; i < 82; i++) {
+            console.log([i]);
+            
+        }
     } else{
-        console.log('caso3')
+        for (let i = 1; i < 50; i++) {
+            console.log([i]);
+            
+        }
     }
 })
